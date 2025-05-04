1a7be587b2e17bce4d4926d2f4b7fae67791818f
CristianAguilarOficial <aguilarcristian060@gmail.com>
2025-05-03 21:17:36 -0500

    agregando cosa para el deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:4000/api', // <-- apunta al prefijo /api
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // <-- apunta al prefijo /api
   withCredentials: true, // si usas cookies para auth
 });
